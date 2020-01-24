6c5ce9348a9646f46e6f8d91120cf374b62e6178
Eyob1984 <tamireyob@gmail.com>
2020-01-23 20:47:15 -0800

    completed location feature

--- server.js
+++ server.js
@@ -1,49 +1,62 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
+
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
+
 //this is our grouping of .get's that will grab our information
-app.get('/', (request, response) => {
-  response.send('This is our Home Page');
-});
-app.get('/wrong', (request, response) => {
-  response.send('OOPS! You did it again. Wrong route.');
-});
-app.get('/location', (request, response) => {
+app.get('/location', locationHandler);
+// app.get('/weather', weatherHandler);
+
+function locationHandler(request, response) {
   try{
-    const geoData = require('./data/geo.json');
-    const city = request.query.city;
-    const locationData = new Location(city, geoData);
-    response.send(locationData);
+    // let city = request.query.city;
+    // let key = process.env.GEOCODE_API_KEY;
+    const url =`https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${request.query.city}&format=json&limit=1`;
+
+    superagent.get(url)
+      .then(data => {
+        console.log(data)
+        const geoData = data.body[0];
+        const locationData = new Location(request.query.city, geoData);
+        response.send(locationData);
+      })
+      .catch(() => {
+        errorHandler('location superagent broke', request, response);
+      });
   }
   catch(error){
-    errorHandler('We are so SOOORRRRRY...something went wrong....', request, response);
+    errorHandler(error, request, response);
   }
-});
-app.get('/weather', (request, response) => {
-  try{
-    const weatherData = require('./data/darksky.json');
-    const forecastData = weatherData.daily.data.map(day => new Weather(day));
-    response.send(forecastData);
-  }
-  catch(error){
-    errorHandler('We are so SOOORRRRRY...something went wrong....', request, response);
-  }
-});
-function Weather(day) {
-  this.time = new Date(day.time*1000).toString().slice(0,15);
-  this.forecast = day.summary;
 }
-function Location(city, geoData){
-  this.searchQuery = city;
-  this.formattedQuery = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitude = geoData[0].lon;
+
+// app.get('/weather', (request, response) => {
+//   try{
+//     const weatherData = require('./data/darksky.json');
+//     const forecastData = weatherData.daily.data.map(day => new Weather(day));
+//     response.send(forecastData);
+//   }
+//   catch(error){
+//     errorHandler('We are so SOOORRRRRY...something went wrong....', request, response);
+//   }
+// });
+// function Weather(day) {
+//   this.time = new Date(day.time*1000).toString().slice(0,15);
+//   this.forecast = day.summary;
+// }
+function Location(query, geoData){
+  this.search_query = query;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
 }
+
 function errorHandler(error, request, response) {
   response.status(500).send(error);
 }
